ea7c3a88fc3c2f5620fe928ad05dfb53e4cceedf
AdniAlatas <adnialatas1@gmail.com>
2024-09-26 23:56:46 +0700

    Last Config

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 //ini javascripts
+
+
+
+function replaceName() {
+    let name = prompt('Masukkan nama anda');
+    console.log(name);
+    document.getElementById('user-name').innerHTML = name;
+}
+
+function validateForm() {
+    let inputName= document.getElementById('input-name').value;
+    if (inputName == '') {
+        alert('Inputan Kosong')
+    } else {
+        document.getElementById('result-form').innerHTML = inputName;
+        alert('Sukses Submit Form');
+    }
+    console.log(inputName);
+}
+
+document.getElementById('submit-btn').addEventListener
